8476084ed9361cbb523903f48d6cd539a37d5105
Tom De Swaef <tom.deswaef@ilvo.vlaanderen.be>
2024-07-30 10:24:03 +0200

    simulation start can be different from plant date (bug fix)

--- R/write_CAL.R
+++ R/write_CAL.R
@@ -7,12 +7,12 @@ write_CAL <- function(Scenario_){
   filename <- paste0("DATA/", Scenario_, ".CAL")
   Input_Date <- Scenario_s %>% dplyr::filter(Scenario == Scenario_) %>% .$Input_Date
   Plant_Date <- Scenario_s %>% dplyr::filter(Scenario == Scenario_) %>% .$Plant_Date
-  plantdate <- (Plant_Date - Input_Date + 1) %>% as.numeric()
+  plantdate <- Plant_Date %>% yday()
   cat('Calendar file ', Scenario_, ' \n',
       '7.0  : AquaCrop Version (August 2022)\n',
       '0    : The onset of the growing period is fixed on a specific date\n',
-      '1    : Day-number (Table 6) of the Start of the time window for the onset criterion\n',
-      '1    : Length (days) of the time window for the onset criterion\n',
+      '-9    : Day-number (Table 6) of the Start of the time window for the onset criterion\n',
+      '-9    : Length (days) of the time window for the onset criterion\n',
       plantdate, '  : Day-number (Table 6) for the onset of the growing period if fixed on a specific date (switch = 0 in line 3)\n',
       '-9   :  Value of the onset criterion (in mm , mm/day, °C, degree-days, or percentage of Rain/ETo ratio)\n',
       '-9   :  Number of successive days for the onset criterion (days)\n',
