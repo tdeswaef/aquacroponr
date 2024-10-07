abc3f43d4f228415dc1eb323d940474f1f116d1c
nielverb <105631532+nielverb@users.noreply.github.com>
2024-10-07 13:43:04 +0200

    linux comp

--- R/02_FileSetup.R
+++ R/02_FileSetup.R
@@ -8,35 +8,43 @@
 #' @returns a character name of the path of the aquacrop.exe file, but also writes the \emph{DailyResults.SIM} file in the appropriate folder. file
 #' @export
 path_config <- function(AquaCrop.path){
-  if(!file.exists("aquacrop.exe")) stop("set your working directory to the AquaCrop.path")
+  if(!any(file.exists(paste0(AquaCrop.path, c("aquacrop", "aquacrop.exe"))))) stop("No executable file found in Aquacrop folder")
 
   AquaCrop.data <- "DATA/" #contains files about weather, soil, crop etc
   AquaCrop.simul <- "SIMUL/" #under this location Mauna loa CO2 file is usually located standard when installing AquaCrop plugin
   AquaCrop.list <- "LIST/" #path where project files are generated for AquaCrop to run
   AquaCrop.output <- "OUTP/" #output path for simulations
 
-  AQ <- paste(AquaCrop.path,"aquacrop.exe",sep = "")# AquaCrop executable
+ # Linux-Windows switch
+  OS <- Sys.info()["sysname"]
 
-  path.data <- gsub('/','\\\\', paste0(AquaCrop.path, AquaCrop.data)) #Creating a windows valid path
-  path.simul <- gsub('/','\\\\', paste0(AquaCrop.path, AquaCrop.simul)) #Creating a windows valid path
-  path.list <- gsub('/','\\\\', paste0(AquaCrop.path, AquaCrop.list)) #Creating a windows valid path
-  path.output <- gsub('/','\\\\',paste0(AquaCrop.path, AquaCrop.output)) #Creating a windows valid path
+  if(OS == "Windows"){
+    AQ <- paste0(AquaCrop.path, "aquacrop.exe") # AquaCrop executable
 
-#create directories if not present
-  if(!dir.exists(path.data)) dir.create(path.data)
+    path.data <- gsub('/','\\\\', paste0(AquaCrop.path, AquaCrop.data)) #Creating a windows valid path
+    path.simul <- gsub('/','\\\\', paste0(AquaCrop.path, AquaCrop.simul)) #Creating a windows valid path
+    path.list <- gsub('/','\\\\', paste0(AquaCrop.path, AquaCrop.list)) #Creating a windows valid path
+    path.output <- gsub('/','\\\\',paste0(AquaCrop.path, AquaCrop.output)) #Creating a windows valid path
+    
+  }else if(OS == "Linux"){
+    AQ <- paste0(AquaCrop.path, "aquacrop") # AquaCrop executable
+
+    path.data <- AquaCrop.data
+    path.simul <-  AquaCrop.simul
+    path.list <- AquaCrop.list
+    path.output <- AquaCrop.output
+
+  }
+
+# create directories if not present
+  if(!dir.exists(path.data)) dir.create(path.data, recursive = T)
   if(!dir.exists(path.simul)) dir.create(path.simul)
   if(!dir.exists(path.list)) dir.create(path.list)
   if(!dir.exists(path.output)) dir.create(path.output)
 
-#define which daily outputs you want from AquaCrop
-  # if(!file.exists(paste(path.simul, "DailyResults.SIM", sep=""))) {
-  # now part of aquacrop_wrapper and aquacrop_morris
-  # }
-
   ## Emptying folders before starting
   unlink("OUTP/*")
   unlink("LIST/*")
-
   return(AQ)
 }
 
